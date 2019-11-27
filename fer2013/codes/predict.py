441017ba32c64e41aae1e087f8656fb73dbcee1b
Adityaojas <45658478+Adityaojas@users.noreply.github.com>
2019-11-27 15:39:59 +0530

    Update predict.py

--- fer2013/codes/predict.py
+++ fer2013/codes/predict.py
@@ -3,28 +3,23 @@ import sys
 
 sys.path.append(config.BASE_PATH)
 
-from mymodule.preprocessor.imagetoarraypreprocessor import ImageToArrayPreprocessor
+from mymodule.preprocessing.imagetoarraypreprocessor import ImageToArrayPreprocessor
 from mymodule.io.hdf5datasetgenerator import HDF5DatasetGenerator
 from keras.preprocessing.image import ImageDataGenerator
+from keras.models import load_model
 
 
 testAug = ImageDataGenerator(rescale = 1/255.0)
 iap = ImageToArrayPreprocessor()
-testGen = HDF5DatasetGenerator(config.HDF5_TEST_PATH, batch_size = config.BATCH_SIZE,
-                               aug = testAug, preprocessor = [iap], classes = 7)
+testGen = HDF5DatasetGenerator(config.HDF5_TEST_PATH, config.BATCH_SIZE,
+                               aug = testAug, preprocessors = [iap], classes = 7)
 
 model = load_model('../checkpoints/best_model.model')
 
 (loss, acc) = model.evaluate_generator(testGen.generator(), steps = testGen.numImages // config.BATCH_SIZE,
-                                         MAX_QUEUE_SIZE = CONFIG.BATCH_SIZE * 2)
+                                         max_queue_size = config.BATCH_SIZE * 2)
 
-PRINT('ACCURACY:{:.2f}'.format(acc*100))
-PRINT('LOSS:{:.2f}'.format(loss*100))
+print('ACCURACY:{:.2f}'.format(acc*100))
+print('LOSS:{:.2f}'.format(loss*100))
 
-testGen.close
-
-
-
-
-
-
+testGen.close()
